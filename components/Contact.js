6a454b6beac4ea36a60f6a17a3a24746539c89c2
keshibat <kensukeshibata@gmail.com>
2022-04-11 14:22:24 +0900

    contact form

--- components/Contact.js
+++ components/Contact.js
@@ -29,7 +29,7 @@ export default function Contact() {
         setSubmitted(true)
         setName('')
         setEmail('')
-        setBody('')
+        setMessage('')
       }
     })
   }
--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,3 +1,30 @@
 export default function (req, res) {
-  console.log(req.body)
-}
+    let nodemailer = require('nodemailer')
+    const transporter = nodemailer.createTransport({
+      port: 465,
+      host: "smtp.gmail.com",
+         auth: {
+              user: 'keshibatmail@gmail.com',
+               pass: process.env.PASSWORD,
+           },
+      secure: true,
+    });
+
+    const mailData = {
+        from: 'keshibatmail@gmail.com',
+        to: 'kensukeshibata@gmail.com',
+        subject: `Message From ${req.body.name}`,
+        text: req.body.message + " | Sent from: " + req.body.email,
+        html: `<div>${req.body.message}</div><p>Sent from: ${req.body.email}</p>`
+    }
+
+    transporter.sendMail(mailData, function (err, info) {
+        if(err)
+          console.log(err)
+        else
+          console.log(info);
+    })
+
+    console.log(req.body)
+    res.send('success')
+  }
